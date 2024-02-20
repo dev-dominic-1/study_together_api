76f0714ed063086c711a721ad170158d7ab2be83
dev-dominic-1 <dominic.dev.email@gmail.com>
2024-02-20 16:06:17 -0600

    Initialized .NET Web API

---
+++ Program.cs
@@ -0,0 +1,38 @@
+var builder = WebApplication.CreateBuilder(args);
+var CorsOrigins = "_CorsOrigins";
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
+builder.Services.AddAuthorization();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: CorsOrigins,
+    policy =>
+    {
+        policy.WithOrigins("http://localhost:8080");
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseCors(CorsOrigins);
+
+// app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
