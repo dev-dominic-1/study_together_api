d650819d0f4a8db729cbadc1a9c8ff13a960a014
Dominic Hiland <dominichiland@Ubuntu-23.10>
2024-02-25 14:30:16 -0600

    Updated gitignore

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -19,7 +19,10 @@ namespace study_together_api.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Post>>> GetAllPosts()
         {
-            return Ok(await _context.Posts.ToListAsync());
+            try
+            { return Ok(await _context.Posts.ToListAsync()); }
+            catch (Exception e)
+            { return StatusCode(500, e); }
         }
     }
 }
